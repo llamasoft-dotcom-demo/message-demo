fd5b31dc99f3b83f7f154bc52a155feaaf1539e6
Joe Ellis <joseph.a.ellis@gmail.com>
2016-02-11 11:39:48 -0500

    New features: +Added message box to top of page +Message box can be minimized by clicking "-" icon +Message box top bar will show quantity of messages, as well as the current state of the message generator ('Idle' or 'Active) +Message box has room to show up to 4 messages (more can be inserted but they'll be hidden out of view.)
    Suggestions
    +Without context, knowing how to improve the messaging is difficult; but in the past when I've added some form of messaging to a page, I've wanted support for different severities of messages.  This would be accomplished through straightforward CSS.
    +Scrolling support might make sense, or a message box that could grow dynamically.  The former creates a scroll bar though (which can be a bit ugly) and the latter either pushes the rest of the page content down as it grows (disorienting) or overlaps it (blocking your view).  That's why I opted for a fixed size, collapsible implementation - but if you really needed room for more messages you might have to consider one of those alternatives.
    +A more efficient implementation could be done by having premade rows instead of using jquery to append and remove "li" tags.  This would be easier for the browser than adding and removing objects to the DOM, but would've complicated the fading animations and required extra javascript to move messages around as messages before and after them timed out - and adding extra code to optimize things before you know you even need to optimize is a great way to bloat a good source file.  Still, something to keep in mind when you do your benchmarking.

--- msg.js
+++ msg.js
@@ -1,15 +1,40 @@
 var loopHandle = null;
 
 // The messageSystem object is where you should do all of your work
-// Use any combination of javascript, HTML and CSS that you feeling
+// Use any combination of javascript, HTML and CSS that you feel
 // is appropriate
-messageSystem = {
-    showMessage: function(msg) {
-        alert(msg);
-    }
-}
+var messageSystem = (function() {
 
+	var msgState = 'Idle';
 
+	var renderMsgCount = function() {
+		var msgCount = $('#message-list li').length;
+		if(msgCount != 0) {
+			var msgCountTxt = msgState + ' - ' + msgCount + ' message' + ((msgCount != 1) ? 's' : '');
+			$('#message-list-count').text(msgCountTxt);
+		} else {
+			$('#message-list-count').text(msgState);
+		}
+	}
+	
+	return {
+		showMessage: function(msg) {
+			var msgElem = $('<li><div class="msg-close noselect">X</div><div>' + msg + '</div></li>');
+			$('#message-list').append(msgElem);
+			msgElem.find('div.msg-close').one('click', function() { msgElem.remove(); renderMsgCount(); } );
+			msgElem.delay(3000).fadeOut(750, function() { 
+				msgElem.remove();
+				renderMsgCount();
+			} );
+			renderMsgCount();
+		},
+		
+		setMsgState(state) {
+			msgState = state;
+			renderMsgCount();
+		}
+	}
+})();
 
 function showMsg() {
     quotes = [
@@ -34,16 +59,29 @@ function loop() {
 
 
 $(function() {
+	messageSystem.setMsgState('Idle');
+
    $('#msgButton').click(function() {
        var btn = $(this),
       btnTxt = btn.text();
        if (btnTxt === 'Start Messages') {
            btn.text('Stop Messages');
+		   messageSystem.setMsgState('Active');
            loopHandle = setTimeout(loop, 500);
        } else {
            btn.text('Start Messages');
            clearTimeout(loopHandle);
+		   messageSystem.setMsgState('Idle');
            loopHandle = null;
        }
    } );
+   
+   $('#message-list-toggle').click(function() {
+		$('#message-list').toggle();
+		if($(this).text() === '-') {
+			$(this).text('+');
+		} else {
+			$(this).text('-');
+		}
+   });
 });
