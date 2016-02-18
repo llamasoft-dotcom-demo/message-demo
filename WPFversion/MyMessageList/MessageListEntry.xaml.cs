62b6ef0b49ccb4ddea94c206a5e914a0700374ab
Jeff Spindler <jeff@perform3-d.com>
2016-02-18 16:40:31 -0500

    Jeff Spindler's Llamasoft quick assessment, done as C# WPF project

---
+++ WPFversion/MyMessageList/MessageListEntry.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MyMessageList
+{
+    /// <summary>
+    /// Control for single entry of MessageListBox
+    /// </summary>
+    public partial class MessageListEntry : UserControl
+    {
+        public MessageListEntry(string text, MessageListBox listBox)
+        {
+            InitializeComponent();
+            _messageText = text;
+            _messageListBox = listBox;
+            _timer.Interval = 3000;
+            _timer.Elapsed += _timer_Elapsed;
+            _timer.Enabled = true;
+            _timer.AutoReset = false;
+        }
+
+        void _timer_Elapsed( object sender, ElapsedEventArgs e )
+        {
+            // would like to fade out here, rather than just remove it
+            _messageListBox.Remove( this );
+        }
+
+        // Reference to parent control
+        private MessageListBox _messageListBox;
+
+        // expiration tiemr
+        private Timer _timer = new Timer();
+
+
+        private string _messageText
+        { 
+            get
+            {
+                return _textBlock.Text;
+            }
+            set
+            {
+                _textBlock.Text = value;
+            } 
+        }
+
+        /// <summary>
+        /// When user clicks button on message, we remove it from the list control
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click( object sender, RoutedEventArgs e )
+        {
+            _messageListBox.Remove( this );
+        }
+    }
+}
---
+++ WPFversion/llama message csharp/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Timers;
+using System.Diagnostics;
+
+namespace llama_message_csharp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer = new Timer( NextMessageMilliseconds() );
+            timer.Elapsed += timer_Elapsed;
+            timer.Enabled = false;
+            timer.AutoReset = true;
+        }
+
+        void timer_Elapsed( object sender, ElapsedEventArgs e )
+        {
+            timer.Interval = NextMessageMilliseconds();
+            timer.Enabled = false;
+
+            string clever = CleverMessage();
+
+            myMsgList.Add( clever );
+            timer.Enabled = true;
+           
+        }
+
+
+        private void Button_Click( object sender, RoutedEventArgs e )
+        {
+            if (LLmsgButton.Content.ToString() == "Start Messages")
+            {
+                LLmsgButton.Content = "Stop Messages";
+                timer.Enabled = true;
+            }
+            else
+            {
+                LLmsgButton.Content = "Start Messages";
+                timer.Enabled = false;
+            }
+        }
+
+        int NextMessageMilliseconds()
+        {
+            return randy.Next( 500, 3000 );
+        }
+
+        string CleverMessage()
+        {
+            int size = CleverMessageList.Count;
+            int choice = randy.Next( 0, size - 1 );
+            return CleverMessageList[choice];
+        }
+
+        List<string> CleverMessageList = new List<string>()
+        {
+            "What we've got here is failure to communicate.",
+            "Go ahead, make my day.",
+            "I've got a bad feeling about this.",
+            "I don't know half of you half as well as I should like; and I like less than half of you half as well as you deserve.",
+            "I find your lack of faith disturbing.",
+            "You're gonna need a bigger boat.",
+            "Tell Mike it was only business.",
+            "I have come here to chew bubble gum and kick ass, and I'm all out of bubble gum."
+        };
+
+        private Timer timer;
+        private Random randy = new Random();
+
+
+    }
+}
