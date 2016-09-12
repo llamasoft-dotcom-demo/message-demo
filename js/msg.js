f9b4a761415018088a52a6ec2be57e63d7b31596
Dave Brunetti <brunetti.dave@gmail.com>
2016-09-11 23:39:07 -0400

    trying an angular version for fun

--- js/msg.js
+++ js/msg.js
@@ -1,98 +1,102 @@
-var loopHandle = null;
+var app = angular.module("message-demo", []);
 
-// The messageSystem object is where you should do all of your work
-// Use any combination of javascript, HTML and CSS that you feeling
-// is appropriate
-messageSystem = {
-    showMessage: function(msg) {
-      //4. creates an alert with the text of the message
-      var notificationType = ["calendar", "reminder", "error", "list", "mail"];
-      var randType = notificationType[(Math.floor(Math.random() * notificationType.length))];
-      $('<div class="' + randType + '"><img src="images/' + randType + '.png" class="icon"><img src="images/x.png" class="x"><h3 class="msg-title">New ' + randType + ' alert:</h3><p class="msg-text">' + msg +'</p>' +'</div>').appendTo('#messages').slideDown('fast');
-      $('#messages div:last-child').delay(3000).slideUp('slow');
+app.controller("make-messages", function($scope, $timeout){
+  var loopHandle = null;
+  $scope.allMessages = [];
+  //$scope.savedMessages = [];
+  $scope.model={
+    messageList: [],
+    savedMessages: []
+  };
+  // $scope.newType = "";
+  // $scope.newMsg = "";
+  $scope.msgIndex = 0;
 
-      $('.x').hover(function(){
-        $(this).parent().css("opacity","0.25");
-        }, function(){
-        $(this).parent().css("opacity","1");
-      });
-      $('.x').on('click',function(){
-        $(this).parent().hide();
-      });
+  var notificationType = ["calendar", "reminder", "error", "list", "mail"];
+  // The messageSystem object is where you should do all of your work
+  // Use any combination of javascript, HTML and CSS that you feel
+  // is appropriate
+  $scope.messageSystem = {
+      showMessage: function(msg) {
+        var randType = notificationType[(Math.floor(Math.random() * notificationType.length))];
+        $scope.canISeeTheMessage = true;
+        $scope.model.messageList.push({
+          randType:randType,
+          msg:msg,
+          dex: $scope.msgIndex
+        });
+        $scope.msgIndex++;
+      },
 
-      $('#messages').children().off().hover(function(){
-        // var currColor = $(this).css("background-color");
-        // var lastComma = currColor.lastIndexOf(',');
-        // var newColor = currColor.substring(0, lastComma+1) + " 1)";
-        // $(this).css("background-color", newColor);
-        $(this).toggleClass('hovered');        
-      }, function(){
-        // var currColor = $(this).css("background-color")
-        // var rgbAColor = currColor.split('(')
-        // var removed = rgbAColor.splice(1, 0, "a(");
-        // rgbAColor = rgbAColor.join('');
-        // var fixColor = rgbAColor.substring(0, rgbAColor.length-1) + ", 1)";
-        // var lastComma = fixColor.lastIndexOf(',');
-        // var newColor = fixColor.substring(0, lastComma+1) + " 0.5)";
-        // $(this).css("background-color", newColor);
-        $(this).toggleClass('hovered');
-      });
+      saveThis: function(context){
+        //$timeout.cancel($scope.threeSeconds);
+        //this works
+        // $(context).parent().appendTo('#saved-reminders');
+        // $scope.canISeeTheMessage = true;
 
-      $('.icon').off().one('click', function(){
-        $(this).parent().clone().appendTo('#saved-reminders').width('275px').addClass('saved').removeClass('hovered').stop().off();
-      });
-    }
-}
+        // console.log($scope.savedMessages);
+        // $scope.newType = angular.element(context).children("#forMessage").attr("ng-class");
+        // $scope.newMsg = angular.element(context).children("p").text();
 
+        // for (var i = 0; i < $scope.model.messageList.length; i++){
+        //   if ($scope.model.messageList[i].dex){
 
+        //   }
+        // }
+        var ind = context.message.dex;
+        console.log(ind);
 
+        $scope.model.savedMessages.push($scope.model.messageList[ind]);
+        console.log($scope.model.savedMessages);
+      },
 
-function showMsg() {
-    quotes = [
-    "We should definitely hire Dave Brunetti. That guy would be an AMAZING fit for our team!",
-    "What we've got here is failure to communicate.",
-    'Go ahead, make my day.',
-    "I've got a bad feeling about this.",
-    "I don't know half of you half as well as I should like; and I like less than half of you half as well as you deserve.",
-    "I find your lack of faith disturbing.",
-    "You're gonna need a bigger boat.",
-    "Tell Mike it was only business.",
-    "I have come here to chew bubble gum and kick ass, and I'm all out of bubble gum."
-    ];
-    //3. passes one of these quotes as the argument in messageSystem.showMessage and calls it
-    messageSystem.showMessage(_.sample(quotes));
-    
-}
-
-function loop() {
-    //2. call showMsg
-    showMsg();
-    //6. ...which sets a random amount of time between 1/2 second and 3 seconds...
-    var rand = Math.round(Math.random() * (3000 - 500)) + 500;
-    //...and calls itself again after rand milliseconds (go back to #2 and repeat until...)
-    loopHandle = setTimeout(loop, rand);
-}
+      hideMe:function(){
+        $timeout(function(){
+          $scope.canISeeTheMessage = false;
+        },3000)
+      }       
+  }
 
 
-$(function() {
-  //1. first they click the button
-   $('#message-reminder').hide();
-   $('#msgButton').on('click',function() {
-      $('#message-reminder').toggle();
-       var btn = $(this),
-      btnTxt = btn.text();
-      //switch text between the two options
-      //if the button starts the messages...
-       if (btnTxt === 'Start Messages') {
-           btn.text('Stop Messages');
-           //...loophandle changes from null to...
-           loopHandle = setTimeout(loop, 500);
-           //which calls the loop function after 1/2 second
-       } else {
-        //7. ...the button gets clicked again, which breaks the cycle!
-           btn.text('Start Messages');
-           clearTimeout(loopHandle);
-           loopHandle = null;
-       }
-   });
+
+
+  function showMsg() {
+      quotes = [
+      "We should definitely hire Dave Brunetti. That guy would be an AMAZING fit for our team!",
+      "What we've got here is failure to communicate.",
+      'Go ahead, make my day.',
+      "I've got a bad feeling about this.",
+      "I don't know half of you half as well as I should like; and I like less than half of you half as well as you deserve.",
+      "I find your lack of faith disturbing.",
+      "You're gonna need a bigger boat.",
+      "Tell Mike it was only business.",
+      "I have come here to chew bubble gum and kick ass, and I'm all out of bubble gum."
+      ];
+      $scope.messageSystem.showMessage(_.sample(quotes));
+      
+  }
+
+  function loop() {
+      showMsg();
+      var rand = Math.round(Math.random() * (3000 - 500)) + 500;
+      loopHandle = $timeout(loop, rand);
+  }
+
+
+  angular.element(document).ready(function() {
+     angular.element(document).find('#message-reminder').hide();
+     angular.element(document).find('#msgButton').on('click',function() {
+        angular.element(document).find('#message-reminder').toggle();
+        var btn = angular.element(document).find(this),
+        btnTxt = btn.text();
+         if (btnTxt === 'Start Messages') {
+             btn.text('Stop Messages');
+             loopHandle = $timeout(loop, 500);
+         } else {
+             btn.text('Start Messages');
+             $timeout.cancel(loopHandle);
+             loopHandle = null;
+         }
+     });
+  });
 });
