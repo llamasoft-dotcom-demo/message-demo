862568ceee8b54c4012c1c11168f4da855442f0d
Andrew Brainerd <drwb333@gmail.com>
2017-01-25 20:01:42 -0500

    Added closing functionality and made style changes

--- js/msg.js
+++ js/msg.js
@@ -1,62 +1,71 @@
+var page, sidebar, popup, msgButton, instructions, msgStatus;
 var loopHandle = null;
-var page, sidebar, popup;
-var count = 0;
+quotes = [
+    "What we've got here is failure to communicate.",
+    'Go ahead, make my day.',
+    "I've got a bad feeling about this.",
+    "I don't know half of you half as well as I should like; and I like less than half of you half as well as you deserve.",
+    "I find your lack of faith disturbing.",
+    "You're gonna need a bigger boat.",
+    "Tell Mike it was only business.",
+    "I have come here to chew bubble gum and kick ass, and I'm all out of bubble gum."
+];
 
 // The messageSystem object is where you should do all of your work
 // Use any combination of javascript, HTML and CSS that you feeling
 // is appropriate
 messageSystem = {
+    numReceived: 0,
+    closeMessage: function(msg) {
+        $("#" + msg.id).fadeOut("fast");
+    },
     showMessage: function (msg) {
-        customAlert(msg);
+        this.numReceived++;
+        var msgID = "not" + this.numReceived;
+        popup = $("<div class='msg' id='" + msgID + "'></div>").append(
+            "<div class='msgClose' onclick='messageSystem.closeMessage(this.parentNode)'>Dismiss</div>" +
+            "<div class='msgBody'>" +
+            "<div class='msgTitle'>Hey, I'm a Quote</div>" +
+            "<div class='msgText'>" + msg + "</div>" +
+            "</div>");
+        sidebar.append(popup);
+        popup.fadeIn("slow", function() {
+            setTimeout(function () {
+                $("#" + msgID).slideUp("slow");
+            }, 10000);
+        });
+        msgStatus.html("Messages Received: " + this.numReceived);
+    },
+    toggleMessages: function() {
+        var btnTxt = msgButton.text();
+        if (btnTxt === "Start Messages") {
+            this.numReceived = 0;
+            msgButton.text("Stop Messages").attr("class", "stop");
+            loopHandle = setTimeout(loop, 500);
+        } else {
+            msgButton.text("Start Messages").attr("class", "start");
+            msgStatus.html("Hit The Button To Get Started!");
+            clearTimeout(loopHandle);
+            loopHandle = null;
+        }
     }
 };
 $(function () {
     page = $("body");
+    instructions = $("#instructions");
+    msgButton = $("#msgButton").addClass("start");
     sidebar = $("#sidebar");
-    $('#msgButton').click(function () {
-        var btn = $(this),
-            btnTxt = btn.text();
-        if (btnTxt === 'Start Messages') {
-            btn.text('Stop Messages');
-            loopHandle = setTimeout(loop, 500);
-        } else {
-            btn.text('Start Messages');
-            clearTimeout(loopHandle);
-            loopHandle = null;
-        }
-    });
-    $("#msgButton").trigger("click");
-});
-function showMsg() {
-    quotes = [
-        "What we've got here is failure to communicate.",
-        'Go ahead, make my day.',
-        "I've got a bad feeling about this.",
-        "I don't know half of you half as well as I should like; and I like less than half of you half as well as you deserve.",
-        "I find your lack of faith disturbing.",
-        "You're gonna need a bigger boat.",
-        "Tell Mike it was only business.",
-        "I have come here to chew bubble gum and kick ass, and I'm all out of bubble gum."
-    ];
-    messageSystem.showMessage(_.sample(quotes));
+    msgStatus = $("#msgStatus");
 
-}
+    msgButton.click(messageSystem.toggleMessages);
+    //s$("#msgButton").trigger("click");
+    /*setTimeout(function() {
+        console.log("Done running");
+        $("#msgButton").trigger("click");
+    }, 10000);*/
+});
 function loop() {
-    showMsg();
+    messageSystem.showMessage(_.sample(quotes));
     var rand = Math.round(Math.random() * (6000)) + 500;
     loopHandle = setTimeout(loop, rand);
-}
-function customAlert(message) {
-    count++;
-    var notiID = "not" + count;
-    popup = $("<div class='notification' id='" + notiID + "'></div>");
-    //popup.append("<div class='notiClose'>Dismiss</div>");
-    popup.append("<div class='notiTitle'>Hey, I'm a Quote</div>");
-    popup.append("<div class='notiText'>" + message + "</div>");
-    sidebar.append(popup);
-    popup.fadeIn("slow", function() {
-        setTimeout(function () {
-            $("#" + notiID).slideUp("slow");
-        }, 5000);
-    });
 }
